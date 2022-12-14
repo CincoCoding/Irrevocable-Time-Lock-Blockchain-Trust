0e6abe07a0a885db249be0d096b41c0e07ae82a9
unknown <davidg.55423@yahoo.com>
2022-12-13 20:32:10 -0600

    front-end skeleton complete using w3.eth functions

--- blockchainTrust/.ipynb_checkpoints/streamlitApp-checkpoint.py
+++ blockchainTrust/.ipynb_checkpoints/streamlitApp-checkpoint.py
@@ -4,6 +4,7 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import web3
 
 load_dotenv()
 
@@ -50,17 +51,91 @@ benificiary1_address = st.sidebar.selectbox("Select Benificiary One", options=ac
 # Use a Streamlit component to get the address of the benificiary two from the user
 benificiary2_address = st.sidebar.selectbox("Select Benificiary Two", options=accounts)
 
+# Display trustee balance
+trustee_balance = str(w3.eth.getBalance(trustee_address))
+st.write("## Trustee Balance: ")
+st.write(f"### {trustee_balance}")
+         
+# Display benificiary1 balance
+benificiary1_balance = str(w3.eth.getBalance(benificiary1_address))
+st.write("## Benificiary One Balance: ")
+st.write(f"### {benificiary1_balance}")
 
-################################################################################
-# Display ouputs tests
-################################################################################
-st.markdown("## Wallet Balances")
+# Display benificiary2 balance
+benificiary2_balance = str(w3.eth.getBalance(benificiary2_address))
+st.write("## Benificiary Two Balance: ")
+st.write(f"### {benificiary2_balance}")
 
-tokens = contract.functions.getBalance().call()
+transaction_amount = st.text_input("Transaction Amount")
 
-st.write(f"This address owns {tokens} ETH. ")
+# Send money from trustee to benificiaries
+## NOTE: USE PYTHON DRY STANDARD HERE AND ABOVE
+if st.button("Send Money"):
+    w3.eth.sendTransaction({"from": str(trustee_address), "to": str(benificiary1_address), "value": int(transaction_amount) })
+    w3.eth.sendTransaction({"from": str(trustee_address), "to": str(benificiary2_address), "value": int(transaction_amount) })
 
-if st.button("Display"):
 
-    st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.J5bwL1q6MkKxAl8goxvinQHaD4%26pid%3DApi&f=1&ipt=2b1d4fde012dae21280d3787977dfdbec9bfc71863f0342153f9cd576b0015d4&ipo=images")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ################################################################################
+# # Display ouputs tests
+# ################################################################################
+# st.markdown("## getBalance")
+# 
+# tokens = contract.functions.getBalance().call()
+# 
+# st.write(f"This address owns {tokens} ETH. ")
+# 
+# 
+# ################################################################################
+# # Display ouputs tests
+# ################################################################################
+# 
+# if st.button("receiveMoney"):
+#     
+#     st.markdown("## receiveMoney")
+#     
+#     tokens2 = contract.functions.receiveMoney().call()
+#     
+#     st.write(f"{tokens2}")
+# 
+# 
+# ################################################################################
+# # Display ouputs tests
+# ################################################################################
+# 
+# if st.button("Withdraw"):
+#     
+#     st.markdown("## Withdraw")
+#     
+#     tokens3 = contract.functions.withdraw().call()
+#     
+#     st.write(f"{tokens3}")
+#     
+# ################################################################################
+# # Display button test
+# ################################################################################
+# if st.button("Display"):
+# 
+#     st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.J5bwL1q6MkKxAl8goxvinQHaD4%26pid%3DApi&f=1&ipt=2b1d4fde012dae21280d3787977dfdbec9bfc71863f0342153f9cd576b0015d4&ipo=images")
+# 
+# ################################################################################
+# # Display ouputs tests
+# ################################################################################
+# 
 
